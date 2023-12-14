6010e893108edcb5cd80cd31c1fa023860428da8
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2023-12-14 23:58:21 +0100

    Update calendar-app.ts

--- calendar-app.ts
+++ calendar-app.ts
@@ -1,19 +1,21 @@
 type Layout_DESKTOP_IFC = {
-    setMenuButton_AEL: Function
+    setMenuButton_AEL: Function,
+    setContentCalendarBox_Width_AEL: Function,
+    setContentCalendarBox_Width_FNC: Function
 };
 class Layout_DESKTOP implements Layout_DESKTOP {
     constructor() {}
     public setMenuButton_AEL(): void {
-        const menuButton: HTMLDivElement = document.querySelector('svg.menu-arrow');
+        const menuButton_EL: HTMLDivElement = document.querySelector('svg.dsk-menu-arrow');
         let isShow: boolean = false;
-        menuButton.addEventListener('click', () => {
-            const sidebar: HTMLElement = document.querySelector('nav.nav-menu');
+        menuButton_EL.addEventListener('click', () => {
+            const sidebar: HTMLElement = document.querySelector('nav.dsk-nav-menu');
             switch (isShow){
                 case false: {
                     isShow = true;
                     setTimeout(() => {
                         sidebar.style.left = '0px';
-                        menuButton.style.transform = 'rotateZ(0deg)';
+                        menuButton_EL.style.transform = 'rotateZ(0deg)';
                         sidebar.style.transitionDuration = '0.5s';
                     }, 1);
                 } break;
@@ -21,19 +23,47 @@ class Layout_DESKTOP implements Layout_DESKTOP {
                     isShow = false;
                     setTimeout(() => {
                         sidebar.style.left = '-250px';
-                        menuButton.style.transform = 'rotateZ(180deg)';
+                        menuButton_EL.style.transform = 'rotateZ(180deg)';
                         sidebar.style.transitionDuration = '0.5s';
                     }, 1);
                 } break;
             }
         }, false);
     }
+    public setContentCalendarBox_Width_AEL(): void {
+        let sidebar_LEFT: number = -250;
+        let sidebar_LEFT_PRP: number = 0;
+        ['load', 'resize'].forEach((ev) => {
+            window.addEventListener(ev, () => {   
+                sidebar_LEFT = document.querySelector('nav.dsk-nav-menu').getBoundingClientRect().left;
+                sidebar_LEFT_PRP = (sidebar_LEFT === -250) ? 0 : 250;
+                let time: string = '0.0s';
+                this.setContentCalendarBox_Width_FNC(sidebar_LEFT_PRP, time);
+            }, false);
+        });
+        const menuButton_EL: HTMLDivElement = document.querySelector('svg.dsk-menu-arrow');
+        menuButton_EL.addEventListener('click', () => {
+            sidebar_LEFT = document.querySelector('nav.dsk-nav-menu').getBoundingClientRect().left;
+            sidebar_LEFT_PRP = (sidebar_LEFT === -250) ? 250 : 0;
+            let time: string = '0.5s';
+            this.setContentCalendarBox_Width_FNC(sidebar_LEFT_PRP, time);
+        }, false);
+    }
+    private setContentCalendarBox_Width_FNC(sidebar_LEFT_PRP, time):void {const calenContBox_EL: HTMLDivElement = document.querySelector('div.dsk-calendar-box');
+        let questBox_WIDTH: number = document.querySelector('div.dsk-quest-box').getBoundingClientRect().width;
+        let result_WIDTH: number = (window.innerWidth - questBox_WIDTH - sidebar_LEFT_PRP);
+        calenContBox_EL.style.width = result_WIDTH + 'px';
+        calenContBox_EL.style.transitionDuration = time;
+        /*console.log('window.innerWidth: ' + window.innerWidth + ' | questBox_WIDTH: ' + questBox_WIDTH + ' | sidebar_LEFT_PRP: ' + sidebar_LEFT_PRP);
+        console.log('result: ' + result_WIDTH);
+        console.log('sidebar_LEFT: ' + sidebar_LEFT);*/
+    }
 };
 
 
 
 type Layout_IFC = {
-    setAppBody_AEL: Function,
+    setAppBody_Height_AEL: Function,
     setLayout_DESKTOP: Function
 };
 // MEGA WAŻNE!
@@ -42,7 +72,7 @@ type Layout_IFC = {
 // konstruktor jest potrzebny.
 class Layout implements Layout_IFC {
     constructor() {}
-    public setAppBody_AEL(): void {
+    public setAppBody_Height_AEL(): void {
         const appBody_EL: HTMLDivElement = document.querySelector('div.app-body');
         ['load', 'resize'].forEach((ev) => {
             addEventListener(ev, () => {
@@ -53,6 +83,7 @@ class Layout implements Layout_IFC {
     setLayout_DESKTOP(): void {
         const layout_DESKTOP: Layout_DESKTOP = new Layout_DESKTOP();
         layout_DESKTOP.setMenuButton_AEL();
+        layout_DESKTOP.setContentCalendarBox_Width_AEL();
     }
 };
 
@@ -68,9 +99,9 @@ type App_IFC = {
     Main: Function
 };
 class App implements App_IFC {
-    public Main() {
+    public Main(): void {
         const layout: Layout = new Layout();
-        layout.setAppBody_AEL();
+        layout.setAppBody_Height_AEL();
         layout.setLayout_DESKTOP();
     }
 };
